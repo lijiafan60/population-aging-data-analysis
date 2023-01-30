d8a48a9b832fc28492b034efab92d1ab892a4cc1
jiafan.li <3151342895@qq.com>
2023-01-31 01:46:58 +0800

    initial pension page
    Signed-off-by: jiafan.li <3151342895@qq.com>

--- src/views/pension.vue
+++ src/views/pension.vue
@@ -1,6 +1,36 @@
 <template>
-  <div>
-    pension
+  <div class="body-box bg-color-black">
+    <dv-border-box12 style="width: 500px">
+    <div class="left-box">
+      <div class="left-2">
+        <div class="outside">
+          <span class="outside-text">导航</span>
+        </div>
+        <div class="inside">
+          <span class="inside-text">导肮1</span>
+        </div>
+        <div class="inside">
+          <span class="inside-text">导航2</span>
+        </div>
+      </div>
+      <div class="left-2">
+        <div class="outside">
+          <span class="outside-text">导航</span>
+        </div>
+        <div class="inside">
+          <span class="inside-text">导肮3</span>
+        </div>
+        <div class="inside">
+          <span class="inside-text">导航4</span>
+        </div>
+      </div>
+    </div>
+    </dv-border-box12>
+    <div class="right-box">
+      <dv-border-box-13>
+
+      </dv-border-box-13>
+    </div>
   </div>
 </template>
 
@@ -11,4 +41,40 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+.body-box {
+  height: 930px;
+  margin-top: 16px;
+  display: flex;
+  flex-direction: row;
+
+  .left-box {
+    width: 500px;
+    display: grid;
+    grid-template-rows: repeat(2, 50%);
+    margin-right: 20px;
+    .left-2 {
+      margin-top: 120px;
+      margin-left: 100px;
+    }
+    .outside {
+      .outside-text {
+        font-size: xxx-large;
+      }
+    }
+    .inside {
+      margin-top: 30px;
+      margin-left: 50px;
+      .inside-text {
+        font-size: x-large;
+      }
+    }
+  }
+
+  .right-box {
+    width: 1390px;
+  }
+}
+.bg-color-black {
+  border-radius: 10px;
+}
 </style>

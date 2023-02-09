b1ee6c83582403450dfaf2ca1ce12e4cb9b829a2
李佳帆 <3151342895@qq.com>
2023-02-10 00:16:18 +0800

    complete service page
    Signed-off-by: 李佳帆 <3151342895@qq.com>

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -1,15 +1,80 @@
 <template>
-  <div>
-    养老服务
+  <div class="body-box">
+    <div class="content-box">
+      <dv-border-box6>
+        <div class="bg-color-black">
+            <mechanism-understanding-degree/>
+        </div>
+      </dv-border-box6>
+      <dv-border-box6>
+        <div class="bg-color-black">
+          <mechanism-impression/>
+        </div>
+      </dv-border-box6>
+      <dv-border-box6>
+        <div class="bg-color-black">
+          <number-in-city/>
+        </div>
+      </dv-border-box6>
+    </div>
+    <div class="content-box">
+      <dv-border-box6>
+        <div class="bg-color-black">
+          <retirement-place/>
+        </div>
+      </dv-border-box6>
+      <dv-border-box6>
+        <div class="bg-color-black">
+          <main-carer/>
+        </div>
+      </dv-border-box6>
+      <dv-border-box6>
+        <div class="bg-color-black">
+          <number-in-countryside/>
+        </div>
+      </dv-border-box6>
+    </div>
   </div>
 </template>
 
 <script>
+import NumberInCity from "@/views/service/mechanism/NumberInCity.vue";
+import NumberInCountryside from "@/views/service/mechanism/NumberInCountryside.vue";
+import MechanismUnderstandingDegree from "@/views/service/mechanism/UnderstandingDegree.vue";
+import MechanismImpression from "@/views/service/mechanism/Impression.vue";
+import RetirementPlace from "@/views/service/place.vue";
+import MainCarer from "@/views/service/carer.vue";
+
 export default {
-  name: "service_page"
+  name: "service_page",
+  components: {
+    MainCarer,
+    RetirementPlace, MechanismImpression, MechanismUnderstandingDegree, NumberInCountryside, NumberInCity
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+.body-box {
+  height: 930px;
+  margin-top: 16px;
+  display: grid;
+  grid-template-rows: repeat(2, 50%);
+  grid-row-gap: 10px;
 
+  .content-box {
+    justify-content: center;
+    display: grid;
+    grid-template-columns: repeat(3, 32%);
+    grid-column-gap: 10px;
+
+    .bg-color-black {
+      height: 432px;
+      border-radius: 10px;
+      margin: 16px;
+
+    }
+  }
+
+}
 </style>
---
+++ src/views/service/mechanism/Impression.vue
@@ -0,0 +1,67 @@
+<template>
+  <div>
+    <EChart width="580px" height="420px" :options="options"></EChart>
+  </div>
+</template>
+
+<script>
+
+import EChart from "@/common/echart/index.vue";
+
+export default {
+  name: "mechanism-impression",
+  components: {EChart},
+  data() {
+    return {
+      options: {
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['总人数', '在职人数', '离职人数']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        xAxis: {
+          type: 'category',
+          name: '年份',
+          boundaryGap: false,
+          data: ['2016', '2017', '2018', '2019', '2020', '2021', '2022']
+        },
+        yAxis: {
+          type: 'value',
+          name: '人数（千万）'
+        },
+        series: [
+          {
+            name: '总人数',
+            type: 'line',
+            stack: 'Total',
+            data: [10, 15, 12.5, 5, 5, 5, 5]
+          },
+          {
+            name: '在职人数',
+            type: 'line',
+            stack: 'Total',
+            data: [22, 18, 19, 23, 29, 33, 31]
+          },
+          {
+            name: '离职人数',
+            type: 'line',
+            stack: 'Total',
+            data: [15, 23, 21, 15, 19, 13, 10]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>

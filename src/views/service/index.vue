b9355b5e00ed85960947c0db953614952725d702
李佳帆 <3151342895@qq.com>
2023-02-20 20:21:32 +0800

    chore: backup

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -1,45 +1,40 @@
 <template>
   <div class="body-box">
-    <div class="content-box">
-      <dv-border-box6>
-        <div class="bg-color-black">
+    <div class="left-body-box">
+      <div class="content-box">
+        <dv-border-box6>
+          <div class="bg-color-black">
             <mechanism-understanding-degree/>
-        </div>
-      </dv-border-box6>
-      <dv-border-box6>
-        <div class="bg-color-black">
-          <mechanism-impression/>
-        </div>
-      </dv-border-box6>
-      <dv-border-box6>
-        <div class="bg-color-black">
-          <number-in-city/>
-        </div>
-      </dv-border-box6>
+          </div>
+        </dv-border-box6>
+        <dv-border-box6>
+          <div class="bg-color-black">
+            <mechanism-impression/>
+          </div>
+        </dv-border-box6>
+      </div>
+      <div class="content-box">
+        <dv-border-box6>
+          <div class="bg-color-black">
+            <retirement-place/>
+          </div>
+        </dv-border-box6>
+        <dv-border-box6>
+          <div class="bg-color-black">
+            <main-carer/>
+          </div>
+        </dv-border-box6>
+      </div>
     </div>
-    <div class="content-box">
+    <div class="right-body-box">
       <dv-border-box6>
-        <div class="bg-color-black">
-          <retirement-place/>
-        </div>
-      </dv-border-box6>
-      <dv-border-box6>
-        <div class="bg-color-black">
-          <main-carer/>
-        </div>
-      </dv-border-box6>
-      <dv-border-box6>
-        <div class="bg-color-black">
-          <number-in-countryside/>
-        </div>
+        <div class="bg-color-black"></div>
       </dv-border-box6>
     </div>
   </div>
 </template>
 
 <script>
-import NumberInCity from "@/views/service/mechanism/NumberInCity.vue";
-import NumberInCountryside from "@/views/service/mechanism/NumberInCountryside.vue";
 import MechanismUnderstandingDegree from "@/views/service/mechanism/UnderstandingDegree.vue";
 import MechanismImpression from "@/views/service/mechanism/Impression.vue";
 import RetirementPlace from "@/views/service/place.vue";
@@ -49,30 +44,46 @@ export default {
   name: "service_page",
   components: {
     MainCarer,
-    RetirementPlace, MechanismImpression, MechanismUnderstandingDegree, NumberInCountryside, NumberInCity
+    RetirementPlace, MechanismImpression, MechanismUnderstandingDegree
   }
 }
 </script>
 
 <style lang="scss" scoped>
+
 .body-box {
   height: 930px;
   margin-top: 16px;
   display: grid;
-  grid-template-rows: repeat(2, 50%);
-  grid-row-gap: 10px;
+  grid-template-columns: 2.3fr 1fr;
 
-  .content-box {
-    justify-content: center;
+  .left-body-box {
     display: grid;
-    grid-template-columns: repeat(3, 32%);
-    grid-column-gap: 10px;
+    grid-template-rows: repeat(2, 49%);
+    grid-row-gap: 10px;
+
+    .content-box {
+      justify-content: center;
+      display: grid;
+      grid-template-columns: repeat(2, 49%);
+      grid-column-gap: 10px;
+
+      .bg-color-black {
+        height: 430px;
+        border-radius: 10px;
+        margin: 16px;
+      }
+    }
+  }
+
+  .right-body-box {
+    display: grid;
+    margin-left: 20px;
 
     .bg-color-black {
-      height: 432px;
+      height: 910px;
       border-radius: 10px;
-      margin: 16px;
-
+      margin: 16px
     }
   }
 
